5cd713db9d49cc2dfa23be9f8d7c329c438473e1
shchai <shchai@hpc2.mesocentre.uca.fr>
2020-03-31 12:15:21 +0200

    test.py

--- EIT_train/test.py
+++ EIT_train/test.py
@@ -3,22 +3,30 @@ import numpy as np
 import torch
 import pickle as pk
 from gen_model import *
+import sys
+import os
 
 # load pca from file
 with open("pca.pkl", "rb") as f:
     pca = pk.load(f)
-
+with open("norm_factor.pkl","rb") as f:
+    norm_factor_img, norm_factor_bdr = pk.load(f)
+original_shape = (266,256)
 # load model from file
-ndim_tot = 2156
+ndim_x, ndim_y, ndim_z, ndim_tot = 900, 256, 1000, 2156
 model = Model(ndim_tot * 2, ndim_tot)
-i_epoch = 1000
-model.load_state_dict(torch.load("./checkpoint/model"+str(i_epoch)+".torch"))
+i_epoch = sys.argv[1]
+ckpt_dir = "./checkpoints/"
+model.load_state_dict(torch.load(ckpt_dir + "model"+i_epoch+".torch"))
+img_dir = "./img/"
+if not os.path.exists(img_dir):
+    os.makedirs(img_dir)
 
 N_samp = 10
 img = np.load("dataImages.npy")
 boundary = np.load("dataBoundary.npy")
 x_samps = img[:N_samp]
-y_samps = boundary[:N_samp]
+y_samps = torch.from_numpy(boundary[:N_samp]).float()
 y_samps = torch.cat([torch.randn(N_samp, ndim_z),
                      torch.zeros(N_samp, ndim_tot - ndim_y - ndim_z),
                      y_samps], dim=1)
@@ -32,5 +40,5 @@ for sample_index in range(N_samp):
     axs[0].set_title('recovered img')
     axs[1].imshow(sample_img)
     axs[1].set_title('original img')
-    fig.savefig("Result"+str(sample_index)+".png")
+    fig.savefig(img_dir + "Result"+str(sample_index)+".png")
     fig.clf()
